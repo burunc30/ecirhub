3067eeacfc89726d8770b1a484fa8fa96b1ff400
burunc30 <e.necefov1985@gmail.com>
2025-05-07 07:55:44 +0400

    Delete leagues.py

--- leagues.py
+++
@@ -1,5 +0,0 @@
-accepted_leagues = [
-    "Albania 1. Division", "Austria 2. Liga", "Belgium First Amateur Division",
-    "Bosnia & Herzegovina First League", "Bulgaria Second League"
-    # Add more as needed
-]
