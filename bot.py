542a373403c477842e43201c12ebacf0b0591bb1
burunc30 <e.necefov1985@gmail.com>
2025-05-07 07:55:27 +0400

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def send_message(text):
-    TOKEN = "YOUR_BOT_TOKEN"
-    CHAT_ID = "YOUR_CHAT_ID"
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "HTML"}
-    requests.post(url, data=payload)
