501548352b639230d6cdab8c991316f94af13109
burunc30 <e.necefov1985@gmail.com>
2025-05-09 12:08:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-# Main match filtering logic will run here
+from bot import send_message
+
+# Test mesajı
+send_message("Test mesajı: Burunc30 sistemi Heroku üzərində uğurla işə düşdü!")
 print('Burunc30 system running')
