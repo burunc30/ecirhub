7a8a5a0cba3755e09b3f8d9dcdd4b0e2448465b9
burunc30 <e.necefov1985@gmail.com>
2025-05-10 18:10:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from bot import send_message
+import requests
 
-# Test mesajı
-send_message("Test mesajı: Burunc30 sistemi Heroku üzərində uğurla işə düşdü!")
-print('Burunc30 system running')
+print("Burunc30 system starting...")
+
+try:
+    response = send_message("Test mesajı: Burunc30 sistemi Heroku üzərində uğurla işə düşdü!")
+    print("Mesaj göndərildi.")
+except requests.exceptions.RequestException as e:
+    print(f"Xəta: {e}")
+
+print("Burunc30 system running.")
