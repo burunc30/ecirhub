7def83949a4ec44ae19330da603f287c8c57d8e6
burunc30 <e.necefov1985@gmail.com>
2025-05-11 16:25:51 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
-
 import requests
 
 def send_message(text):
     TOKEN = "8106341353:AAFVdOReCiCnhdqHq-F0z3rjf-89YrHso8E"
     CHAT_ID = "1488455191"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "HTML"}
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
     requests.post(url, data=payload)
