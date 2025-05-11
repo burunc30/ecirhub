fbf711cc439c7e9932987ccd3df1f499354114f0
burunc30 <e.necefov1985@gmail.com>
2025-05-11 16:13:59 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,9 @@
+
 import requests
 
 def send_message(text):
     TOKEN = "8106341353:AAFVdOReCiCnhdqHq-F0z3rjf-89YrHso8E"
     CHAT_ID = "1488455191"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": text,
-        "parse_mode": "HTML"
-    }
-    response = requests.post(url, data=payload)
-    print(f"Telegram cavabı: {response.status_code}, {response.text}")
-    if response.status_code == 200:
-        print("Telegram mesajı uğurla göndərildi.")
-    else:
-        print("Telegram mesajı göndərilə bilmədi.")
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "HTML"}
+    requests.post(url, data=payload)
