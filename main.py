c61a668b6150d099b21fb60360519d098bd80d6a
burunc30 <e.necefov1985@gmail.com>
2025-05-11 11:10:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from bot import send_message
 
 print('Burunc30 system starting...')
-
-try:
-    send_message("Test mesajı: Burunc30 sistemi Heroku üzərində uğurla işə düşdü!")
-    print('Telegram mesajı uğurla göndərildi.')
-except Exception as e:
-    print(f'Telegram mesajı göndərilə bilmədi. Xəta: {e}')
+send_message("Test mesajı: Burunc30 sistemi Heroku üzərində uğurla işə düşdü!")
