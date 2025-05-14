d92d3221265db20d40104a04d3b76014af700cb3
burunc30 <e.necefov1985@gmail.com>
2025-05-14 08:06:42 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,9 @@
-import time
 import requests
 
-def run_bot():
-    TOKEN = "8106341353:AAFIi3nfPOlydtCM_eYHiSIbDR0C1RFoaG4"  # Yeni Telegram bot tokenin
-    CHAT_ID = "1488455191"  # Sənin Telegram chat ID-in
+TOKEN = "8106341353:AAFIi3nfPOlydtCM_eYHiSIbDR0C1RFoaG4"
+CHAT_ID = "1488455191"
 
-    while True:
-        text = "Burunc30 sistemi işləyir!"
-        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-        payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
-        try:
-            response = requests.post(url, data=payload)
-            print(f"Mesaj göndərildi: {response.status_code}")
-        except Exception as e:
-            print(f"Xəta baş verdi: {e}")
-        time.sleep(600)  # 10 dəqiqə gözləyir
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {"chat_id": CHAT_ID, "text": text, "parse_mode": "Markdown"}
+    requests.post(url, data=payload)
