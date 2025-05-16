634a528df41b1d0f9a8aa02526e126e492a158b9
burunc30 <e.necefov1985@gmail.com>
2025-05-16 20:59:31 +0400

    Delete scarping_1xbet.py

--- scarping_1xbet.py
+++
@@ -1,6 +0,0 @@
-
-def get_1xbet_matches():
-    return [
-        {"home": "Chelsea", "away": "Arsenal", "odds": {"1": 2.10, "X": 3.20, "2": 3.40}, "dnb": {"1": 1.80, "2": 1.90}, "btts": "Yes", "under2_5": "Yes", "htft": {"1/1": 3.5, "2/2": 4.2, "1/2": 28.0}},
-        {"home": "Real Madrid", "away": "Barcelona", "odds": {"1": 1.85, "X": 3.40, "2": 4.0}, "dnb": {"1": 1.40, "2": 2.60}, "btts": "Yes", "under2_5": "No", "htft": {"1/1": 2.5, "2/2": 5.5, "1/2": 19.0}},
-    ]
